33b559c712fc98dc00995dfbd297e3fa8e0d5a1c
Pol-black <165894882+Pol-black@users.noreply.github.com>
2026-05-13 21:33:43 +0300

    Add files via upload

---
+++ Algorithm_Cannon.cpp
@@ -0,0 +1,308 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <mpi.h>
+#include <ctime>
+#include <chrono> 
+#include <cmath>
+
+using namespace std;
+using namespace std::chrono;
+
+// Глобальные переменные
+int ProcNum = 0;       // Общее число процессов
+int ProcRank = 0;      // Ранг текущего процесса
+int GridSize;          // Размер квадратной решетки (число процессов по одной оси)
+int GridCoords[2];     // Координаты процесса в решетке
+MPI_Comm GridComm;     // Коммуникатор всей решетки
+
+// Прототипы функций
+void CreateGridCommunicators();
+void ProcessInitialization(double*& pAMatrix, double*& pBMatrix, double*& pCMatrix,
+    double*& pAblock, double*& pBblock, double*& pCblock, int& Size, int& BlockSize);
+void RandomDataInitialization(double* pAMatrix, double* pBMatrix, int Size);
+void DataDistribution(double* pAMatrix, double* pBMatrix, double* pAblock, double* pBblock,
+    int Size, int BlockSize);
+void InitialShift(double* pAblock, double* pBblock, int BlockSize);
+void BlockMultiplication(double* pAblock, double* pBblock, double* pCblock, int BlockSize);
+void ShiftLeft(double* pAblock, int BlockSize);
+void ShiftUp(double* pBblock, int BlockSize);
+void ResultCollection(double* pCMatrix, double* pCblock, int Size, int BlockSize);
+void ProcessTermination(double* pAMatrix, double* pBMatrix, double* pCMatrix,
+    double* pAblock, double* pBblock, double* pCblock);
+
+// Функция для красивого вывода матрицы
+void PrintMatrix(double* matrix, int size, int max_rows = 6, int max_cols = 6) {
+    for (int i = 0; i < min(size, max_rows); ++i) {
+        for (int j = 0; j < min(size, max_cols); ++j) {
+            cout.width(7);
+            cout.precision(2);
+            cout << fixed << matrix[i * size + j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Создание коммуникатора в виде двумерной решетки
+void CreateGridCommunicators() {
+    int dims[2] = { GridSize, GridSize };
+    int periods[2] = { 1, 1 }; // Циклические сдвиги важны для Кэннона
+    int reorder = 1;
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, reorder, &GridComm);
+    MPI_Cart_coords(GridComm, ProcRank, 2, GridCoords);
+}
+
+// Инициализация параметров задачи и выделение памяти
+void ProcessInitialization(double*& pAMatrix, double*& pBMatrix, double*& pCMatrix,
+    double*& pAblock, double*& pBblock, double*& pCblock,
+    int& Size, int& BlockSize) {
+
+    if (ProcRank == 0) {
+        do {
+            cout << "\nВведите размер матриц (кратный " << GridSize << "): ";
+            cin >> Size;
+            if (Size % GridSize != 0)
+                cout << "Размер должен быть кратен " << GridSize << "!\n";
+        } while (Size % GridSize != 0);
+    }
+
+    MPI_Bcast(&Size, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    BlockSize = Size / GridSize;
+
+    pAblock = new double[BlockSize * BlockSize];
+    pBblock = new double[BlockSize * BlockSize];
+    pCblock = new double[BlockSize * BlockSize];
+
+    // Обнуляем результат перед началом
+    for (int i = 0; i < BlockSize * BlockSize; i++)
+        pCblock[i] = 0.0;
+
+    if (ProcRank == 0) {
+        pAMatrix = new double[Size * Size];
+        pBMatrix = new double[Size * Size];
+        pCMatrix = new double[Size * Size];
+        RandomDataInitialization(pAMatrix, pBMatrix, Size);
+
+        cout << "\nМатрица A (первые 6x6):\n";
+        PrintMatrix(pAMatrix, Size);
+
+        cout << "\nМатрица B (первые 6x6):\n";
+        PrintMatrix(pBMatrix, Size);
+    }
+}
+
+// Заполнение матриц случайными данными
+void RandomDataInitialization(double* pAMatrix, double* pBMatrix, int Size) {
+    srand(42); // Для воспроизводимости
+    for (int i = 0; i < Size * Size; i++) {
+        pAMatrix[i] = (double)(rand() % 10);
+        pBMatrix[i] = (double)(rand() % 10);
+    }
+}
+
+// Распределение блоков матриц по процессам
+void DataDistribution(double* pAMatrix, double* pBMatrix,
+    double* pAblock, double* pBblock,
+    int Size, int BlockSize) {
+
+    int numBlocks = GridSize;
+
+    if (ProcRank == 0) {
+        for (int i = 0; i < numBlocks; i++) {
+            for (int j = 0; j < numBlocks; j++) {
+                int coords[2] = { i, j };
+                int rank;
+                MPI_Cart_rank(GridComm, coords, &rank);
+
+                if (rank == 0) {
+                    for (int ii = 0; ii < BlockSize; ii++) {
+                        for (int jj = 0; jj < BlockSize; jj++) {
+                            pAblock[ii * BlockSize + jj] = pAMatrix[(i * BlockSize + ii) * Size + j * BlockSize + jj];
+                            pBblock[ii * BlockSize + jj] = pBMatrix[(i * BlockSize + ii) * Size + j * BlockSize + jj];
+                        }
+                    }
+                }
+                else {
+                    double* bufferA = new double[BlockSize * BlockSize];
+                    double* bufferB = new double[BlockSize * BlockSize];
+
+                    for (int ii = 0; ii < BlockSize; ii++) {
+                        for (int jj = 0; jj < BlockSize; jj++) {
+                            bufferA[ii * BlockSize + jj] = pAMatrix[(i * BlockSize + ii) * Size + j * BlockSize + jj];
+                            bufferB[ii * BlockSize + jj] = pBMatrix[(i * BlockSize + ii) * Size + j * BlockSize + jj];
+                        }
+                    }
+
+                    MPI_Send(bufferA, BlockSize * BlockSize, MPI_DOUBLE, rank, 0, MPI_COMM_WORLD);
+                    MPI_Send(bufferB, BlockSize * BlockSize, MPI_DOUBLE, rank, 1, MPI_COMM_WORLD);
+
+                    delete[] bufferA;
+                    delete[] bufferB;
+                }
+            }
+        }
+    }
+    else {
+        MPI_Status status;
+        MPI_Recv(pAblock, BlockSize * BlockSize, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, &status);
+        MPI_Recv(pBblock, BlockSize * BlockSize, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD, &status);
+    }
+}
+
+// Начальный сдвиг блоков для алгоритма Кэннона
+void InitialShift(double* pAblock, double* pBblock, int BlockSize) {
+    MPI_Status status;
+    int src, dst;
+
+    // Сдвиг A влево на GridCoords[0]
+    int shiftA = GridCoords[0];
+    MPI_Cart_shift(GridComm, 1, -shiftA, &src, &dst);
+    MPI_Sendrecv_replace(pAblock, BlockSize * BlockSize, MPI_DOUBLE, dst, 0, src, 0, GridComm, &status);
+
+    // Сдвиг B вверх на GridCoords[1]
+    int shiftB = GridCoords[1];
+    MPI_Cart_shift(GridComm, 0, -shiftB, &src, &dst);
+    MPI_Sendrecv_replace(pBblock, BlockSize * BlockSize, MPI_DOUBLE, dst, 0, src, 0, GridComm, &status);
+}
+
+// Умножение двух блоков матриц
+void BlockMultiplication(double* pAblock, double* pBblock, double* pCblock, int BlockSize) {
+    for (int i = 0; i < BlockSize; i++) {
+        for (int j = 0; j < BlockSize; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < BlockSize; k++) {
+                sum += pAblock[i * BlockSize + k] * pBblock[k * BlockSize + j];
+            }
+            pCblock[i * BlockSize + j] += sum;
+        }
+    }
+}
+
+// Циклический сдвиг блока A влево
+void ShiftLeft(double* pAblock, int BlockSize) {
+    MPI_Status status;
+    int src, dst;
+    MPI_Cart_shift(GridComm, 1, -1, &src, &dst);
+    MPI_Sendrecv_replace(pAblock, BlockSize * BlockSize, MPI_DOUBLE, dst, 0, src, 0, GridComm, &status);
+}
+
+// Циклический сдвиг блока B вверх
+void ShiftUp(double* pBblock, int BlockSize) {
+    MPI_Status status;
+    int src, dst;
+    MPI_Cart_shift(GridComm, 0, -1, &src, &dst);
+    MPI_Sendrecv_replace(pBblock, BlockSize * BlockSize, MPI_DOUBLE, dst, 0, src, 0, GridComm, &status);
+}
+
+// Сборка результата на процессе 0
+void ResultCollection(double* pCMatrix, double* pCblock, int Size, int BlockSize) {
+    if (ProcRank == 0) {
+        int myRow = GridCoords[0];
+        int myCol = GridCoords[1];
+
+        for (int i = 0; i < BlockSize; i++) {
+            for (int j = 0; j < BlockSize; j++) {
+                pCMatrix[(myRow * BlockSize + i) * Size + (myCol * BlockSize + j)] = pCblock[i * BlockSize + j];
+            }
+        }
+
+        for (int proc = 1; proc < ProcNum; proc++) {
+            int coords[2];
+            MPI_Cart_coords(GridComm, proc, 2, coords);
+            double* buffer = new double[BlockSize * BlockSize];
+            MPI_Status status;
+            MPI_Recv(buffer, BlockSize * BlockSize, MPI_DOUBLE, proc, 2, MPI_COMM_WORLD, &status);
+
+            for (int i = 0; i < BlockSize; i++) {
+                for (int j = 0; j < BlockSize; j++) {
+                    pCMatrix[(coords[0] * BlockSize + i) * Size + (coords[1] * BlockSize + j)] = buffer[i * BlockSize + j];
+                }
+            }
+
+            delete[] buffer;
+        }
+    }
+    else {
+        MPI_Send(pCblock, BlockSize * BlockSize, MPI_DOUBLE, 0, 2, MPI_COMM_WORLD);
+    }
+}
+// Освобождение памяти
+void ProcessTermination(double* pAMatrix, double* pBMatrix, double* pCMatrix,
+    double* pAblock, double* pBblock, double* pCblock) {
+
+    if (ProcRank == 0) {
+        delete[] pAMatrix;
+        delete[] pBMatrix;
+        delete[] pCMatrix;
+    }
+
+    delete[] pAblock;
+    delete[] pBblock;
+    delete[] pCblock;
+}
+
+
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+    srand(static_cast<unsigned int>(time(0)));
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;  // переменные замера времени
+    double* pAMatrix = NULL, * pBMatrix = NULL, * pCMatrix = NULL;
+    double* pAblock = NULL, * pBblock = NULL, * pCblock = NULL;
+    int Size, BlockSize;
+
+    setvbuf(stdout, 0, _IONBF, 0); // Отключить буферизацию
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    if (ProcRank == 0) cout << "Параллельное умножение матриц (алгоритм Кэннона)" << endl;
+
+    GridSize = (int)sqrt((double)ProcNum);
+    if (ProcNum != GridSize * GridSize) {
+        if (ProcRank == 0)
+            cout << "Число процессов должно быть полным квадратом!" << endl;
+        MPI_Finalize();
+        return 1;
+    }  
+
+    // Создание 2D коммуникаторов
+    CreateGridCommunicators();
+        
+    // Инициализация данных и выделение памяти
+    ProcessInitialization(pAMatrix, pBMatrix, pCMatrix, pAblock, pBblock, pCblock, Size, BlockSize);
+    if (ProcRank == 0) start = high_resolution_clock::now(); // начинаем отсчёт времени после иннициализации
+
+    // Распределение блоков матриц между процессами
+    DataDistribution(pAMatrix, pBMatrix, pAblock, pBblock, Size, BlockSize);
+
+    // Начальный сдвиг блоков матриц
+    InitialShift(pAblock, pBblock, BlockSize);
+
+    // Основной цикл алгоритма Кэннона
+    for (int iter = 0; iter < GridSize; iter++) {
+        BlockMultiplication(pAblock, pBblock, pCblock, BlockSize);
+        ShiftLeft(pAblock, BlockSize);
+        ShiftUp(pBblock, BlockSize);
+    }
+
+    // Сборка результата на процессе 0
+    ResultCollection(pCMatrix, pCblock, Size, BlockSize);
+
+
+    // Вывод результата
+    if (ProcRank == 0) {
+        // остановка таймера перед выводом результата
+        end = high_resolution_clock::now(); 
+        auto duration_milli = duration_cast<milliseconds>(end - start).count();
+
+        cout << "\nРезультат умножения (первые 6x6):\n";
+        PrintMatrix(pCMatrix, Size);
+
+        cout << "\n\nВремя работы программы: " << duration_milli << " мс" << endl << endl;
+    }
+
+    // Освобождение памяти
+    ProcessTermination(pAMatrix, pBMatrix, pCMatrix, pAblock, pBblock, pCblock);
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ Algorithm_Linear.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <mpi.h>
+#include <ctime>
+#include <chrono> 
+using namespace std;
+using namespace std::chrono;
+
+// Инициализация случайных данных в матрицах A и B
+void RandomDataInitialization(double* pAMatrix, double* pBMatrix, int Size) {
+    for (int i = 0; i < Size * Size; i++) {
+        pAMatrix[i] = (double)(rand() % 10);
+        pBMatrix[i] = (double)(rand() % 10);
+    }
+}
+
+// Умножение блока A на всю матрицу B, запись в блок C
+void MultiplyBlock(double* Ablock, double* B, double* Cblock, int rows, int N) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < N; j++) {
+            double sum = 0.0;
+            for (int k = 0; k < N; k++) {
+                sum += Ablock[i * N + k] * B[k * N + j];
+            }
+            Cblock[i * N + j] = sum;
+        }
+    }
+}
+
+// Вывод матрицы (как прямоугольник)
+void PrintMatrix(double* matrix, int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout.width(7); 
+            cout.precision(2);
+            cout << fixed << matrix[i * cols + j] << " ";
+        }
+        cout << endl;
+    }
+}
+int main(int argc, char* argv[]) {
+    setlocale(LC_ALL, "Russian");
+    srand(static_cast<unsigned int>(time(0)));
+    int ProcNum, ProcRank;
+    int N;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
+    double* A = NULL, * B = NULL, * C = NULL;
+    double* Ablock = NULL, * Cblock = NULL;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+
+    if (ProcRank == 0) cout << "Параллельное умножение матриц (Линейный алгоритм)" << endl;
+    if (ProcRank == 0) {
+        do {
+            cout << "Введите размер квадратных матриц (кратный числу процессов " << ProcNum << "): ";
+            cin >> N;
+            if (N % ProcNum != 0)
+                cout << "Размер должен быть кратен числу процессов\n";
+        } while (N % ProcNum != 0);
+    }
+    MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    int rows_per_proc = N / ProcNum;
+
+    // Все процессы выделяют память под B
+    B = (double*)malloc(N * N * sizeof(double));
+    if (!B) {
+        cerr << "Ошибка выделения памяти для B на процессе " << ProcRank << endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    if (ProcRank == 0) {
+        A = (double*)malloc(N * N * sizeof(double));
+        C = (double*)calloc(N * N, sizeof(double)); 
+        if (!A || !C) {
+            cerr << "Ошибка выделения памяти на процессе 0\n";
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        RandomDataInitialization(A, B, N);
+
+        cout << "Матрица A:\n";
+        PrintMatrix(A, min(6, N), min(6, N));
+        cout << "Матрица B:\n";
+        PrintMatrix(B, min(6, N), min(6, N));
+        start = high_resolution_clock::now();
+    }
+// Выделяем память под блоки
+    Ablock = (double*)malloc(rows_per_proc * N * sizeof(double));
+    Cblock = (double*)malloc(rows_per_proc * N * sizeof(double));
+    if (!Ablock || !Cblock) {
+        cerr << "Ошибка выделения памяти под блоки на процессе " << ProcRank << endl;
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    // Рассылка данных
+    if (ProcNum > 1) {
+        MPI_Scatter(A, rows_per_proc * N, MPI_DOUBLE,
+            Ablock, rows_per_proc * N, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+        MPI_Bcast(B, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    }
+    else {
+        // Если один процесс — просто копируем всё в Ablock
+        for (int i = 0; i < N * N; ++i)
+            Ablock[i] = A[i];
+    }
+    //Параллельное умножение матриц
+    MultiplyBlock(Ablock, B, Cblock, rows_per_proc, N);
+
+    //Сборка результата на процессе 0 
+    if (ProcNum > 1) {
+        MPI_Gather(Cblock, rows_per_proc * N, MPI_DOUBLE,
+            C, rows_per_proc * N, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    }
+    else {
+        for (int i = 0; i < N * N; ++i)
+            C[i] = Cblock[i];
+    }
+// Вывод результата
+    if (ProcRank == 0) {
+        end = high_resolution_clock::now();  //остановка таймера перед выводом результата
+        cout << "Умножение матриц завершено.\n";
+        cout << "Результат C (первые 6x6):\n";
+        PrintMatrix(C, min(6, N), min(6, N));
+
+        free(A);
+        free(B);
+        free(C);
+        auto duration_milli = duration_cast<milliseconds>(end - start).count();
+        cout << "\n\nВремя работы программы: " << duration_milli << " мс" << endl;
+    }
+    free(Ablock);
+    free(Cblock);
+    MPI_Finalize();
+    return 0;
+}
